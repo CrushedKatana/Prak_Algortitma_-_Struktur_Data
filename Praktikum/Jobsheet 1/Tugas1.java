e2d27232f97d9dcfcabe008cfac744151d2a4a53
CrushedKatana <charelsadewo123@gmail.com>
2024-02-16 13:34:16 +0700

    Commit tugas 1

--- Praktikum/Jobsheet 1/Tugas1.java
+++ Praktikum/Jobsheet 1/Tugas1.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class Tugas1 {
     public static void main(String[] args) {
-        String[] KODE = {"B", "D", "F", "H", "K", "L", "N", "P", "R", "T"};
-        String[] KOTA = {"Bandung", "Depok", "Surabaya", "Jakarta", "Kediri", "Solo", "Malang", "Padang", "Semarang", "Tangerang"};
+        String[] Kode = {"A", "B", "C", "D", "E", "G", "H", "L", "N", "T"};
+        String[] Kota = {"Banten", "Jakarta", "Bandung", "Cirebon", "Bogor", "Pekalongan", "Semarang", "Surabaya",  "Malang", "Tegal"};
         int numPlates = 10;
 
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Masukkan kode plat mobil: ");
+        String inputKode = scanner.nextLine();
 
+        String namaKota = cariNamaKota(inputKode, Kode, Kota, numPlates);
+        System.out.println("Kota: " + namaKota);
+    }
 
+    
+    public static String cariNamaKota(String kode, String[] Kode, String[] Kota, int numPlates) {
+        for (int i = 0; i < numPlates; ++i) {
+            if (kode.equals(Kode[i])) {
+                return Kota[i];
+            }
+        }
+        return "Kota tidak ditemukan";
     }
 }
