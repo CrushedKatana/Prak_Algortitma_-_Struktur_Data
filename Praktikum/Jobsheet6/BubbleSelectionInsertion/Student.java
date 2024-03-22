5f4a697496949e46d309ea006a6ac8946e5ee5b2
CrushedKatana <charelsadewo123@gmail.com>
2024-03-22 13:04:52 +0700

    Create Student.java

---
+++ Praktikum/Jobsheet6/BubbleSelectionInsertion/Student.java
@@ -0,0 +1,15 @@
+package Praktikum.Jobsheet6.BubbleSelectionInsertion;
+
+public class Student {
+    String name;
+    int entraceYear, age;
+    double gpa;
+
+    Student (String n, int y, int a, double g){
+        name = n;
+        entraceYear = y;
+        age = a;
+
+    
+    }
+}
