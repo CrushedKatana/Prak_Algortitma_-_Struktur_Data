ed60bd46a42c7d3dbe24c9936e4edd365d62dd59
CrushedKatana <charelsadewo123@gmail.com>
2024-04-02 20:56:04 +0700

    co

--- Praktikum/Jobsheet6/Assigment/Ticket.java
+++ Praktikum/Jobsheet6/Assigment/Ticket.java
@@ -1,5 +1,24 @@
 package Praktikum.Jobsheet6.Assigment;
 
 public class Ticket {
+    String airlines;
+    int price;
+    String destination;
+    String origin;
 
+    public Ticket(String airlines, String destination, String origin, int price) {
+        this.airlines = airlines;
+        this.destination = destination;
+        this.origin = origin;
+        this.price = price;
+    }
+
+    public String toString() {
+        return "Ticket{" +
+                "airlines='" + airlines + '\'' +
+                ", price=" + price +
+                ", destination='" + destination + '\'' +
+                ", origin='" + origin + '\'' +
+                '}';
+    }
 }
--- Praktikum/Jobsheet6/Assigment/TicketService.java
+++ Praktikum/Jobsheet6/Assigment/TicketService.java
@@ -1,5 +1,74 @@
 package Praktikum.Jobsheet6.Assigment;
 
-public class TicketService {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TicketService {
+    List<Ticket> tickets;
+
+    public TicketService() {
+        this.tickets = new ArrayList<>();
+    }
+
+    public void add(Ticket t) {
+        tickets.add(t);
+    }
+
+    public void displayAll() {
+        for (Ticket ticket : tickets) {
+            System.out.println(ticket);
+        }
+    }
+
+    public void bubbleSortAscending() {
+        for (int i = 0; i < tickets.size() - 1; i++) {
+            for (int j = 0; j < tickets.size() - i - 1; j++) {
+                if (tickets.get(j).price > tickets.get(j + 1).price) {
+                    Ticket temp = tickets.get(j);
+                    tickets.set(j, tickets.get(j + 1));
+                    tickets.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    public void bubbleSortDescending() {
+        for (int i = 0; i < tickets.size() - 1; i++) {
+            for (int j = 0; j < tickets.size() - i - 1; j++) {
+                if (tickets.get(j).price < tickets.get(j + 1).price) {
+                    Ticket temp = tickets.get(j);
+                    tickets.set(j, tickets.get(j + 1));
+                    tickets.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    public void selectionSortAscending() {
+        for (int i = 0; i < tickets.size() - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < tickets.size(); j++) {
+                if (tickets.get(j).price < tickets.get(minIndex).price) {
+                    minIndex = j;
+                }
+            }
+            Ticket temp = tickets.get(minIndex);
+            tickets.set(minIndex, tickets.get(i));
+            tickets.set(i, temp);
+        }
+    }
+
+    public void selectionSortDescending() {
+        for (int i = 0; i < tickets.size() - 1; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < tickets.size(); j++) {
+                if (tickets.get(j).price > tickets.get(maxIndex).price) {
+                    maxIndex = j;
+                }
+            }
+            Ticket temp = tickets.get(maxIndex);
+            tickets.set(maxIndex, tickets.get(i));
+            tickets.set(i, temp);
+        }
+    }
 }
