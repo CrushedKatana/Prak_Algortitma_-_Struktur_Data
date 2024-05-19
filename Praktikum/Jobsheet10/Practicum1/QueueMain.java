d2f4b866e7b81e8e0cad7ad65ec847ffaa7ccb4a
CrushedKatana <charelsadewo123@gmail.com>
2024-05-19 23:52:33 +0700

    commit

---
+++ Praktikum/Jobsheet10/Practicum1/QueueMain.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class QueueMain {
+    
+    public static void menu(){
+        System.out.println("Choose menu: ");
+        System.out.println("1. Enqueue");
+        System.out.println("2. Dequeue");
+        System.out.println("3. First");
+        System.out.println("4. Peek");
+        System.out.println("5. Clear");
+        System.out.println("=====================");
+    }
+    public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Insert maximum queue : ");
+    int n  = sc.nextInt();
+
+    Queue Q = new Queue(n);
+
+    int choose;
+    do{
+        menu();
+        choo
+    }
+    }
+}
